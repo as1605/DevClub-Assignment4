39b94553d2cd174a71191d0841cc03715ca9771b
Ashaeyx <96980688+Ashaeyx@users.noreply.github.com>
2022-03-31 23:37:44 +0530

    Add files via upload

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,27 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+const login_element = document.querySelector(".loginpanel"); // Fill the selector for the login element in ""
 let login_text = login_element.innerText;
 
-let question = ""; // Use split and array operations on the login_text string to extract the question
+let quest = login_text.split(' '); // Use split and array operations on the login_text string to extract the question
+console.log(quest);
+let ans=0;
+if (quest[1+5]==='add'){
+    ans=Number(quest[2+5])+Number(quest[4+5]);
+}
+if (quest[1+5]=='subtract'){
+    ans=Number(quest[2+5])-Number(quest[4+5]);
+}
+if (quest[1+5]=='enter'){
+    if (quest[2+5]=='second'){
+        ans=quest[6+5];
+    }
+    if (quest[2+5]=='first'){
+        ans=quest[4+5];
+    }
+}
+console.log(ans);
+ // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
 
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
-
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
-captcha_input_element.value = answer;
+const captcha_input_element = document.querySelector("#valuepkg3"); // Fill the selector for the captcha input element in ""
+captcha_input_element.value = ans;
+document.getElementById("loginbtn").submit();
